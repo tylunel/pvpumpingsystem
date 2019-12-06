6a3953a026a9d86eb01d818b2d4343f2b0ba8fa0
Lunel <tanguy.lunel.1@ens.etsmtl.ca>
2019-12-06 16:36:20 -0500

    - tests for setting CI

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+MAINTAINER_EMAIL='tanguy.lunel.1@ens.etsmtl.ca'
+AUTHORS='Tanguy LUNEL'
+
+setup(
+   name='pvpumpingsystem',
+   version='0.1',
+   description='Module for simulating off-grid photovoltaic powered pumping station',
+   license=GPL3,
+   author=AUTHORS,
+   author_email=MAINTAINER_EMAIL,
+   url=https://github.com/tylunel/pvpumpingsystem,
+   packages=['pvpumpingsystem'],  #same as name
+   install_requires=['numpy', 'pandas', 'scipy', 'pvlib'], #external packages as dependencies
+)

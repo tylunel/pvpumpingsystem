6573350551b3d498df33753b545eb7f59cb70c89
Lunel <tanguy.lunel.1@ens.etsmtl.ca>
2020-01-06 14:58:04 -0500

    - changes in configuration of sphinx doc build

--- docs/conf.py
+++ docs/conf.py
@@ -27,9 +27,6 @@ class Mock(MagicMock):
 MOCK_MODULES = []
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
-import pandas as pd
-pd.show_versions()
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
